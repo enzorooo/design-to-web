98f7fc1426dd6992563ef017bab03a6e33bd41d7
enzorooo <ph.enzomercado@gmail.com>
2022-09-25 14:16:42 +0800

    Add App.js connection to compontnets and containers

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React from 'react';
- 
+
+import { About, Blog, Features, Footer, Header, Possibility  } from 'containers/.';
+import { CTA, Brand, Navbar } from './components';
+
+  
 const App = () => {
   return (
     <div>
